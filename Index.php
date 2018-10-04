9faf317ea62c532eb1ffb2427ae07e112daae14a
Viktoras Test <vmilovanov@telecentras.lt>
2018-10-04 15:57:58 +0300

    asfasf

--- Index.php
+++ Index.php
@@ -1,9 +1,7 @@
 <?
 echo "Hello world";
 echo "Hello me";
-<<<<<<< HEAD
-=======
 echo "Specialus pakeitimas";
 echo "Specialus pakeitimas nes kitas nepavyko";
->>>>>>> 19a557960c5740e0ab1e654e2fb086a828f6fbe7
+
 ?>
